823e945bab5f78d23b8418f97daea52d9b532c1a
MaXwel25 <VMVMaxim@mail.ru>
2026-04-29 22:10:06 +0300

    update para23.04

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    id("com.google.devtools.ksp")
+
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -48,12 +49,19 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
 
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation("com.google.code.gson:gson:2.10.1")
+
+
     implementation("androidx.room:room-runtime:2.8.4")
     implementation("androidx.room:room-ktx:2.8.4")
     implementation("androidx.compose.material3:material3:1.4.0")
     // implementation("androidx.room:room-compiler:2.8.4")
     ksp("androidx.room:room-compiler:2.8.4")
-    //ksp(libs.androidx.room.compiler)
+    ksp(libs.androidx.room.compiler)
+
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3") // or latest version
     implementation("androidx.core:core-ktx:1.13.1")
--- app/src/main/java/com/example/martpara/composeUI/Form.kt
+++ app/src/main/java/com/example/martpara/composeUI/Form.kt
@@ -1,4 +1,155 @@
 package com.example.martpara.composeUI
 
-class Form {
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
+import android.icu.util.Calendar
+import android.widget.DatePicker
+import android.widget.TimePicker
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.example.martpara.MainViewModel
+import com.example.martpara.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DateInputField(viewModel: MainViewModel) {
+    val context = LocalContext.current
+    val calendar = Calendar.getInstance()
+    val year = calendar.get(Calendar.YEAR)
+    val month = calendar.get(Calendar.MONTH)
+    val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+    val datePickerDialog = DatePickerDialog(
+        context,
+        { _: DatePicker, selectedYear: Int, selectedMonth: Int, selectedDay: Int ->
+            viewModel.date =
+                "${checkZero(selectedDay)}:${checkZero(selectedMonth + 1)}"
+        },
+        year, month, day
+    )
+
+    Box {
+        OutlinedTextField(
+            value = viewModel.date,
+            onValueChange = { viewModel.date = it },
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable { datePickerDialog.show() },
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedContainerColor = colorResource(R.color.navy),
+                unfocusedContainerColor = colorResource(R.color.navy),
+                disabledContainerColor = colorResource(R.color.navy),
+                errorContainerColor = colorResource(R.color.navy),
+            ),
+            enabled = false
+        )
+        Text(
+            text = if (viewModel.date.isNotEmpty()) viewModel.date
+            else stringResource(R.string.choose_date),
+            color = colorResource(R.color.blue),
+            modifier = Modifier
+                .align(Alignment.CenterStart)
+                .padding(start = 10.dp)
+        )
+    }
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TimeInputField(viewModel: MainViewModel) {
+    val context = LocalContext.current
+    val calendar = Calendar.getInstance()
+    val hour = calendar.get(Calendar.HOUR_OF_DAY)
+    val minute = calendar.get(Calendar.MINUTE)
+
+    val timePickerDialog = TimePickerDialog(
+        context,
+        { _: TimePicker, selectedHour: Int, selectedMinute: Int ->
+            viewModel.time =
+                "${checkZero(selectedHour)}:${checkZero(selectedMinute)}"
+        },
+        hour, minute, true
+    )
+
+    Box {
+        OutlinedTextField(
+            value = viewModel.time,
+            onValueChange = { viewModel.time = it },
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable { timePickerDialog.show() },
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedContainerColor = colorResource(R.color.navy),
+                unfocusedContainerColor = colorResource(R.color.navy),
+                disabledContainerColor = colorResource(R.color.navy),
+                errorContainerColor = colorResource(R.color.navy),
+            ),
+            enabled = false
+        )
+        Text(
+            text = if (viewModel.time.isNotEmpty()) viewModel.time
+            else stringResource(R.string.choose_time),
+            color = colorResource(R.color.blue),
+            modifier = Modifier
+                .align(Alignment.CenterStart)
+                .padding(start = 10.dp)
+        )
+    }
+}
+
+@Composable
+fun CreateButton(onClick: () -> Unit) {
+    val keyboardController = LocalSoftwareKeyboardController.current
+    Button(
+        onClick = {
+            onClick()
+            keyboardController?.hide()
+        },
+        modifier = Modifier
+            .padding(bottom = 10.dp, start = 10.dp, end = 10.dp)
+            .fillMaxWidth()
+            .background(
+                brush = Brush.horizontalGradient(
+                    colors = listOf(
+                        colorResource(id = R.color.button_gradient_purple),
+                        colorResource(id = R.color.button_gradient_blue)
+                    )
+                ),
+                shape = RoundedCornerShape(15.dp)
+            ),
+        colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent)
+    ) {
+        Text(
+            text = stringResource(id = R.string.form_create),
+            style = TextStyle(
+                color = Color.White,
+                fontWeight = FontWeight.Bold
+            )
+        )
+    }
+}
+
+// Вспомогательная функция для форматирования чисел с нулём
+private fun checkZero(value: Int): String = if (value < 10) "0$value" else value.toString()
